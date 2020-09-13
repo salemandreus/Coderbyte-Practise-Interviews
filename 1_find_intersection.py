23a478c5ca0d3087d36ac36b948f5eead9ee076f
salemandreus <salemandreus@gmail.com>
2020-09-13 21:01:09 +0200

    Exercise 1 - Works on my machine
    Doesn't work on their platform for some reason, see Todo

---
+++ 1_find_intersection.py
@@ -0,0 +1,18 @@
+def FindIntersection(strArr):
+
+    # Format into int sets
+    mystrings = strArr.strip('"]["').replace(" ","").split('","')
+    first, second = [set(map(int, mystring.split(","))) for mystring in mystrings]
+
+    # Get intersection
+    return list(first & second)
+
+print(FindIntersection(input()))
+
+# Test Input:
+# ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+
+# TODO:
+# This works on my pycharm running Python 3.8!
+# But somehow this doesn't work on the site - test cases and possibly running the code seems to
+# do an array conversion in the background when using Python 3 - this should not be happening 🤔

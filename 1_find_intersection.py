7011004f227f1b9afa5267540f1cf23050b771e9
salemandreus <salemandreus@gmail.com>
2020-09-14 09:43:27 +0200

    Add Pi WIP (commented)

--- 1_find_intersection.py
+++ 1_find_intersection.py
@@ -1,19 +1,35 @@
-# WRONG
+def FindIntersection(strArr):
+
+    # Format into int sets
+    mystrings = strArr.strip('"]["').replace(" ","").split('","')
+    first, second = [set(map(int, mystring.split(","))) for mystring in mystrings]
+
+    # Get intersection
+    return list(first & second)
+
+print(FindIntersection(input()))
+
+# Test Input:
+# ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+
+# TODO:
+# This works locally running Python 3.8!
+# But somehow this doesn't work on the Coderbyte site - test cases and possibly running the code seems to
+# do a list conversion in the background when using Python 3 - this should not be happening 🤔
+
+
+# Test Input:
+# ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+
+
+# # Pi WIP
 # def FindIntersection(strArr):
 #
-#     # Format into int sets
-#     mystrings = strArr.strip('"]["').replace(" ","").split('","')
-#     first, second = [set(map(int, mystring.split(","))) for mystring in mystrings]
+#     [a, b] = [set(s.split(", ")) for s in strArr]
 #
-#     # Get intersection
-#     return list(first & second)
+#     return ",".join(a & b)
 #
 # print(FindIntersection(input()))
 #
-# # Test Input:
-# # ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
 #
-# # TODO:
-# # This works locally running Python 3.8!
-# # But somehow this doesn't work on the Coderbyte site - test cases and possibly running the code seems to
-# # do a list conversion in the background when using Python 3 - this should not be happening 🤔
+# #return ",".join(sorted(a & b, key=int))
